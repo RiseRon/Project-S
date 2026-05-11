322ed5312e64094617f578aaf47e706fb10dc82d
RiseRon <cat051113@gmail.com>
2026-05-11 16:20:42 +0900

    슬라임(카드) 배치 (완료)
    카드로 된 슬라임을 드래그해서 슬롯에 배치 가능
    슬라임 드래그 중에 슬라임이 적 공격 불가능

--- Assets/_Project/Scripts/Entities/Slime/SlimeCard.cs
+++ Assets/_Project/Scripts/Entities/Slime/SlimeCard.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class SlimeCard : MonoBehaviour
+public class SlimeCard : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     // 이 카드가 들고 있을 슬라임 데이터
     public SO_SlimeData slimeData;
@@ -12,5 +13,31 @@ public class SlimeCard : MonoBehaviour
 
         // 여기서 나중에 UI 텍스트(이름, 공격력 등)를 업데이트할 수 있습니다.
         // 예: nameText.text = data.slimeName;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        // PlacementManager에 카드 자신(this)을 함께 넘겨줍니다.
+        PlacementManager.Instance.StartSummonDrag(slimeData, this);
+
+        // 드래그 시작 시 카드는 숨깁니다.
+        gameObject.SetActive(false);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        // 3D 슬라임 이동은 PlacementManager에서 처리하므로 비워둡니다.
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        // 배치가 끝나면 PlacementManager에서 처리하도록 유도하거나 
+        // 로직에 따라 여기서 추가 처리를 합니다.
+    }
+
+    // 배치 실패 시 다시 카드를 보여주는 함수
+    public void OnPlacementFailed()
+    {
+        gameObject.SetActive(true);
     }
 }
